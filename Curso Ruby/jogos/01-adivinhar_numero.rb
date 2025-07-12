d01cb53b3dc6b071cfa68d094f18ad8fa3c6c937
André <88333841+andresena47@users.noreply.github.com>
2025-07-11 23:53:17 -0300

    Add files via upload

--- Curso Ruby/jogos/01-adivinhar_numero.rb
+++ Curso Ruby/jogos/01-adivinhar_numero.rb
@@ -24,7 +24,7 @@ jogo = AdivinharNumero.new
 until jogo.venceu do
 puts "" 
 puts "" 
-puts "Digite um numero de 1 a 1000"
+puts "Digite um numero de 1 a 10"
 numero = gets.to_i
 
 puts jogo.tentar_adivinhar(numero)
